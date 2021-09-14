5b6a8c822ae2804e78f22517a4a29a7f9a7eb6c1
whao <1601676846@qq.com>
2021-09-15 00:43:01 +0800

    9.14.2021

--- c++基础知识/打印二维数组.cpp
+++ c++基础知识/打印二维数组.cpp
@@ -1,17 +1,50 @@
-#include<iostream>
+#include <iostream>
+#include <stdio.h>
+
 using namespace std;
 
-void Print(int * arr, int m, int n){
+void out(double **a,int m, int n)
+{
+    int i, j;
+    double b=0.0;
+    for(i=0; i<m; i++)
+    {
+        for (j=0; j<n; j++)
+        {
+            a[i][j] = b;
+            b += 1.2;
+            printf("%5.1f",a[i][j]);
+        }
+        std::cout << std::endl;
+    }   
+
+}
+
+void Print(int ** arr, int m, int n){
     for(int i = 0; i<m; i++){
         for(int j = 0; j<n; j++){
-            cout << arr[i*m+j] << " ";
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-int main(){
-    int arr[3][2] = {1,2,3,4,5,6};
-    Print(arr,3,2);
-    return 0;
+int main()
+{
+    int i, j, m=2, n=3;
+    int **a;
+    
+    a = new int*[m];
+    for (i=0; i<m; i++)
+        a[i] = new int[n];
+
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            a[i][j] = i+j;
+        }
+    }
+    
+    Print(a,m,n);
+
+    return 1;
 }
