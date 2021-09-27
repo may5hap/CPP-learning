d7c8bf79e4d8e00faa602cec5625eaa9ab751274
whao <1601676846@qq.com>
2021-09-27 23:40:11 +0800

    9.27.2021

--- linux-system-programming/process/forktest.cpp
+++ linux-system-programming/process/forktest.cpp
@@ -16,9 +16,13 @@ int main(){
         perror("fork error");
     }
     else if(cpid==0){                          // if cpid == 0, it means the current process is the child process
+        a++;
+        cout << "child process" << a << endl;
         cout << "child process---" << " my process id: " << getpid() << " my father process id: " << getppid() << "  " << &a << endl;
     }
     else if(cpid > 0){                          //if cpid is bigger than 0, the current process is the father process and cpid is the child process id
+        a++;
+        cout << "main process" << a << endl; 
         cout << "father process---" << " my process id: " << getpid() << " my father process id: " << getppid() << " my child process:" << cpid << "  " << &a << endl;
     }
     cout << "child and father process" << endl;
